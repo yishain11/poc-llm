7504674605c4a84b8a395eca0e583944869df1c4
yishain11 <yishain11@walla.co.il>
2024-07-17 15:20:58 +0300

    added

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-pipe = pipeline("text-generation", model="TheBloke/WizardLM-7B-uncensored-GPTQ")
-res = pipe("gemerate a python question with 3 wrong answer and 1 correct one, with explanations for each")
-print('res: ', res)
+model_name_or_path = "TheBloke/WizardLM-7B-uncensored-GPTQ"
+# To use a different branch, change revision
+# For example: revision="oobaCUDA"
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
+                                             device_map="auto",
+                                             trust_remote_code=False,
+                                             revision="main")
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+prompt = "Tell me about AI"
+prompt_template=f'''A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {prompt} ASSISTANT:
+
+'''
+
+print("\n\n*** Generate:")
+
+input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
+print(tokenizer.decode(output[0]))
+
+# Inference can also be done using transformers' pipeline
+
+print("*** Pipeline:")
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    do_sample=True,
+    temperature=0.7,
+    top_p=0.95,
+    top_k=40,
+    repetition_penalty=1.1
+)
+
+print(pipe(prompt_template)[0]['generated_text'])
